59096ba4121a58bba8d67e0a5cb76116f5db6372
Guilherme Costa Alves <galves@secullum.intra>
2025-12-29 10:47:33 -0300

    alteracoes novas

--- script/decompilador/index.js
+++ script/decompilador/index.js
@@ -1,3 +1,17 @@
+async function copiarTexto() {
+  // Pega o elemento
+  const content = document.getElementById('areaDaResposta');
+
+  try {
+    // Usa a API moderna de área de transferência
+    await navigator.clipboard.writeText(content.value);
+  } catch (err) {
+    console.error('Falha ao copiar: ', err);
+  }
+}
+
+
+
 function decodificaImagem() {
     const code = document.getElementById('areaDoCodigo').value.trim();
     if (!code) return;
@@ -30,5 +44,51 @@ function baixarImagem() {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+
+}
+
+
+function converteTexto() {
+    const txt = document.getElementById('areaDoCodigo');
+    const resposta = document.getElementById('areaDaResposta');
+
+    let decodeString = atob(txt.value.trim());
+
+    resposta.value = decodeString
+
+}
+
+
+function converteHex() {
+    const textoAreaPrompt = document.getElementById("areaDoCodigo");
+    const textoAreaResposta = document.getElementById("areaDaResposta");
+    // pega o valor em Code64
+    const txt = textoAreaPrompt.value.trim();
+
+
+    let decodeString = atob(txt);
+
+    var hex = '';
+    for (var i = 0; i < decodeString.length; i++) {
+        // Obter o valor Unicode do caractere atual
+        var charCode = decodeString.charCodeAt(i);
+
+        // Converter o valor numérico para hexadecimal e garantir 2 dígitos com padStart
+        var hexValue = charCode.toString(16).padStart(2, '0');
+
+        hex += hexValue;
+    }
+
+    textoAreaResposta.value = hex;
+}
+
+function mudarformatoHex() {
+    const textoAreaResposta = document.getElementById("areaDaResposta");
+
+    const format = textoAreaResposta.value
+        .replace(/(..)/g, '$1 ')
+        .trim();
+
+    textoAreaResposta.value = format;
 
 }
